f30e31f0a9f988ef6a8507e4d66e0b7a08f5843c
Tianyang <thuu0017@student.monash.edu>
2025-09-15 07:16:29 +1000

    feat(router): add firebase auth routes (login, register, logout)

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import FirebaseSigninView from '../views/FirebaseSigninView.vue'
+import FirebaseRegisterView from '../views/FirebaseRegisterView.vue'
+import FirebaseLogoutView from '../views/FirebaseLogoutView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView
-  }
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/FireLogin', name: 'FireLogin', component: FirebaseSigninView },
+  { path: '/FireRegister', name: 'FireRegister', component: FirebaseRegisterView },
+  { path: '/Logout', name: 'Logout', component: FirebaseLogoutView },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
