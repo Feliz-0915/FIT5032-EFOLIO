74ccf4cae3c09562e8e765763feba20e90ec0fd8
Tianyang <thuu0017@student.monash.edu>
2025-10-06 19:08:48 +1100

    Task 9.2

--- functions/index.js
+++ functions/index.js
@@ -7,22 +7,31 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 const {onRequest} = require("firebase-functions/v2/https");
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 const admin = require("firebase-admin");
-const cors = require("cors")({origin: true});
 
 admin.initializeApp();
+const db = admin.firestore();
 
-exports.countBooks = onRequest((req, res) => {
-  cors(req, res, async () => {
-    try {
-      const booksCollection = admin.firestore().collection("books");
-      const snapshot = await booksCollection.get();
-      const count = snapshot.size;
+exports.countBooks = onRequest(async (req, res) => {
+  try {
+    const snapshot = await db.collection("books").get();
+    res.status(200).send({count: snapshot.size});
+  } catch (error) {
+    res.status(500).send({error: error.message});
+  }
+});
 
-      res.status(200).send({count});
-    } catch (error) {
-      console.error("Error counting books:", error.message);
-      res.status(500).send("Error counting books");
+exports.uppercaseBook = onDocumentCreated("books/{bookId}", async (event) => {
+  const data = event.data.data();
+  const upperData = {};
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      upperData[key] = String(data[key]).toUpperCase();
     }
-  });
+  }
+
+  await event.data.ref.set(upperData, {merge: false});
+  console.log("Book data converted to uppercase:", upperData);
 });
